6a82c6f73d405c003e061b27314efab7954dfc8a
daniel bariudin <daniel.bariudin@live.biu.ac.il>
2021-02-16 13:02:43 +0200

    final commit and fix

--- api/routes/dates.js
+++ api/routes/dates.js
@@ -1,48 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const { response } = require('express');
 
 
-class HebrewDateConverter{
 
-    constructor(date) {
-        this.date = date;
-        this.day = '';
-        this.month = '';
-        this.year = '';
-    }
-
-    convertDate() {
-        const date_list = [String(this.date).split('/')];
-        this.day = date_list[0][0];
-        this.month = date_list[0][1];
-        this.year = date_list[0][2];
-
-    }
-
+function parseDate(date) {
+    tokens = [String(date).split('/')];
+    const day = tokens[0][0];
+    const month = tokens[0][1];
+    const year = tokens[0][2];
+    const res = { day: day, month: month, year: year };
+    return res
 }
+
 //date route
 router.get('/api', (req, res, next) => {
-    const model = new HebrewDateConverter(req.query.gregDate);
-    model.convertDate();
-    const url = `https://www.hebcal.com/converter?cfg=json&gy=${model.year.toString()}&gm=${model.month.toString()}&gd=${model.day.toString()}&g2h=1`;
-    axios.get(url).then(
-        (response) => {
-            res.json({
-                hebrew:response.data.hebrew
-            })
-    },
-        (error) => {
-            console.log('error:', error);
+    const date_token = parseDate(req.query.gregDate);
+    const url = `https://www.hebcal.com/converter?cfg=json&gy=${date_token['year']}&gm=${date_token['month']}&gd=${date_token['day']}&g2h=1`;
+    axios.get(url).
+    then((response) => {
+        res.json({
+            hebrew:response.data.hebrew
+        })
+    }).
+    catch((error) => {
+        console.log('error:', error);
     });
 });
+
 
 /*
 I added default routes just in case
 */
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(400).json({
         message: "Please Enter correct url: https://dateconverterbariudin.herokuapp.com/api/{here you enter the date} "
     });
 });
